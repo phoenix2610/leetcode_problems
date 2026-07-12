c3a702fed826a5b21bf057af01812ca95e3da683
phoenix2610 <phoenix2610@users.noreply.github.com>
2026-07-12 15:26:57 +0530

    Add 9. Palindrome Number [Python]

--- Easy/0009-palindrome-number/solution.py
+++ Easy/0009-palindrome-number/solution.py
@@ -1,10 +1,15 @@
-# 9. Palindrome Number
-# Difficulty: Easy
-# https://leetcode.com/problems/palindrome-number/
-# Tags: Math
-
-from typing import List, Optional, Tuple
-
 class Solution:
-    def solution(self):
-        pass
+    def isPalindrome(self, x: int):
+        
+        if x < 0 or (x % 10 == 0 and x != 0) :
+           return False
+
+        halfRev = 0
+
+        while halfRev < x:
+            
+            halfRev = halfRev * 10 + (x % 10)
+            x //= 10
+
+        return halfRev == x or halfRev // 10 == x    
+        
