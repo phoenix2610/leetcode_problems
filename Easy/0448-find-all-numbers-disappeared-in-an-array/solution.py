d187bb948a5e816e784e980ac62299bfe10bffc1
Phoenix Dragon <iamcoming2610@gmail.com>
2026-08-08 10:09:47 +0530

    Implement findDisappearedNumbers function

--- Easy/0448-find-all-numbers-disappeared-in-an-array/solution.py
+++ Easy/0448-find-all-numbers-disappeared-in-an-array/solution.py
@@ -1,10 +1,5 @@
-# 448. Find All Numbers Disappeared in an Array
-# Difficulty: Easy
-# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
-# Tags: Array, Hash Table
-
-from typing import List, Optional, Tuple
-
 class Solution:
-    def solution(self):
-        pass
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        numSet = set(nums)
+        return [i for i in range (1, len(nums) + 1) if i not in numSet]
+        
